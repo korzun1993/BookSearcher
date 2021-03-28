facf13d07b871044554ecff62de3696b0aa35d62
Vladyslav Korzun <korzun.v.a@gmail.com>
2021-03-29 02:42:04 +0300

    Initial Commit

---
+++ BookSearcherTests/BookSearcherTests.swift
@@ -0,0 +1,34 @@
+//
+//  BookSearcherTests.swift
+//  BookSearcherTests
+//
+//  Created by Vladyslav Korzun on 29.03.2021.
+//  Copyright © 2021 VladyslavKorzun. All rights reserved.
+//
+
+import XCTest
+@testable import BookSearcher
+
+class BookSearcherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
